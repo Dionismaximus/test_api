d168f005025099b0f7f9691f04e96d0691548127
Denys Lytvynenko <dionismaximus666@gmail.com>
2024-07-06 11:02:44 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const API_KEY = "XZxrCCvf3qvQk6kwx-j4qncbyoA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data.expiry);
+    }
+    else {
+        throw new Error(data.error)
+    }
+
+}
+
+function displayStatus(data) {
+    let heading = "API Key Status"
+    let results = `<div class="key-status">${data.expiry}</div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerText = results;
+
+    resultsModal.show();
+}
